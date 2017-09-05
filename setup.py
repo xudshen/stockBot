3a9b40c9ddc66dbe4b1ad60398aa6016a6f7ba82
Your Name <you@example.com>
2017-09-05 11:59:57 +0800

    init

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+
+from setuptools import setup
+
+
+# Utility function to read the README file.
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setup(
+    name='stockBot',
+    version='0.0.1',
+    author='BFather',
+    description=('stock bot for guoguo',),
+    license='MIT',
+    keywords='stock bot',
+    url='https://github.com/',
+    long_description=read('README.md'),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
+    requires=['itchat>=1.3.9']
+)
